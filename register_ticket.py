ee00e2b14d347858f5dccd14a2ceb60a90b52b3b
amir reza <esmaeili69315@gmail.com>
2023-05-29 21:34:43 +0330

    creat cunstractor function

--- register_ticket.py
+++ register_ticket.py
@@ -7,3 +7,12 @@ class MyException(Exception):
     pass
 
 class User:
+    def __init__(self, name: str, birthdate: datetime.date, membership_months: int, age_rating: int):
+        """
+        Initialize a User object with the provided attributes.
+        """
+        self.name = name
+        self.birthdate = birthdate
+        self.membership_months = membership_months
+        self.age_rating = age_rating
+
