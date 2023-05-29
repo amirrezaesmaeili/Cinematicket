51bdfbab77445ca851501d8cf22020681b2038c3
amir reza <esmaeili69315@gmail.com>
2023-05-29 21:35:51 +0330

    creat is_birthday function

--- register_ticket.py
+++ register_ticket.py
@@ -15,4 +15,12 @@ class User:
         self.birthdate = birthdate
         self.membership_months = membership_months
         self.age_rating = age_rating
+        
+
+    def is_birthday(self) -> bool:
+        """
+        Check if it's the user's birthday today.
+        """
+        today = datetime.date.today()
+        return today.month == self.birthdate.month and today.day == self.birthdate.day
 
